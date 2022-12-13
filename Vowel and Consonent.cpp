fce03018046c9ebf879536dafcdd5060ddff3638
Moon2340 <farjanamoon20144@gmail.com>
2022-12-13 23:03:48 +0600

    Nested if-else

--- Vowel and Consonent.cpp
+++ Vowel and Consonent.cpp
@@ -5,10 +5,12 @@ int main()
     char ch;
     cout<<"Enter any Letter: ";
     cin>>ch;
-    if (ch>='A'&&ch<='Z'||ch>='a'&&ch<='z')
+    if (ch>='A'&&ch<='Z'||ch>='a'&&ch<='z') //here the main if-else statement.
     {
 
     if (ch=='a'||ch=='i'||ch=='e'||ch=='o'||ch=='u'||ch=='A'||ch=='I'||ch=='O'||ch=='E'||ch=='U')
+      /*This is called nested if-else statement.
+     If in one statement we reuse the statement that is called nested */
         cout<<"The Letter is Vowel";
     else
         cout<<"The Letter is consonent";
