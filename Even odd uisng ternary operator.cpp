5e94205feb62ac6d01abf5cf8909f993c0734cc1
Moon2340 <farjanamoon20144@gmail.com>
2022-12-15 01:34:43 +0600

    define even or odd using ternary operator

---
+++ Even odd uisng ternary operator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter the Number: ";
+    cin>>number;
+   (number%2==0) ? cout<< number <<" is even ": cout<< number <<" is Odd ";
+   return 0;
+}
