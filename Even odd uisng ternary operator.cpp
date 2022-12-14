48c0b435e630e334a4aa34d2198ba44d41c9c33a
Moon2340 <farjanamoon20144@gmail.com>
2022-12-15 01:41:57 +0600

    commented

--- Even odd uisng ternary operator.cpp
+++ Even odd uisng ternary operator.cpp
@@ -5,6 +5,6 @@ int main()
     int number;
     cout<<"Enter the Number: ";
     cin>>number;
-   (number%2==0) ? cout<< number <<" is even ": cout<< number <<" is Odd ";
+   (number%2==0) ? cout<< number <<" is even ": cout<< number <<" is Odd "; // ? works as if and : works as else.
    return 0;
 }
